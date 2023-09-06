8bd36def7fb9cc3227d377363b4a46391d789bd1
enioncaushi <enioncaushi@hotmail.com>
2023-09-06 15:21:45 +0200

    firstt

---
+++ first JSX/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+ 
+
+  return (
+    <>
+      
+  <h1>Hello Dojo! </h1>
+  <p>Things I need to do: </p>
+  <ul>
+  <li>Learn React</li>
+  <li>Climb Mt. Everest</li>
+  <li>Run a marathon</li>
+  <li>Feed the dogs</li>
+  </ul>
+
+    </>
+  )
+}
+
+export default App
